0fe1d62c905025b916b45b4aca5409445265aa75
Sean <skabe89@gmail.com>
2021-10-13 21:55:47 -0400

    board and tile

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,15 @@
 import React, {useState} from 'react'
+import Tile from './Tile'
 
 export default function Board() {
+
+  const [board, setBoard] = useState(['1', '2', '3', '4', '5', '6', '7', '8', '0'])
+
+
   return (
     <div>
-      
+      {console.log({board})}
+      <Tile char = {board[4]} />
     </div>
   )
 }
---
+++ src/components/Tile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Tile( {char} ) {
+  return (
+    <div>
+      {char}
+    </div>
+  )
+}
